87c88e38c1aafda98790365cfd618ecafbc8ed98
Ben Ford <ben.ford@puppetlabs.com>
2020-07-07 15:22:06 -0700

    Update lib/puppet_blacksmith/modulefile.rb
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- lib/puppet_blacksmith/modulefile.rb
+++ lib/puppet_blacksmith/modulefile.rb
@@ -25,7 +25,7 @@ module Blacksmith
       metadata['name'].split('-',2)[0]
     end
     def author
-      metadata['author'] || metadata['name'].split('-',2)[0]
+      metadata['author'] || namespace
     end
     def version
       metadata['version']
